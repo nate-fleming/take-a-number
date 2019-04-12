b15dae07d291d1460446f07ae2970cdad605ce11
nate-fleming <nfleming4@gmail.com>
2019-04-12 10:43:28 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+console.log('hello');
+
+let bandNumber = 0
+
+const takeNumber = function (band) {
+    return `${bandNumber += 1}. ${band}`
+}
+
+const scum = takeNumber("Galactic Scum")
+console.log(scum)  // This should print "1. Galactic Scum" in the console
+
+const under = takeNumber("Underdogs")
+console.log(under)  // This should print "2. Underdogs" in the console
